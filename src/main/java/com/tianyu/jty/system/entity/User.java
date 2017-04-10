5599a54dced45048425ea6e39bb29eb1867ab257
Allen.Wang <Allen.Wang@lionbridge.com>
2017-04-10 17:34:14 +0800

    add comment

--- src/main/java/com/tianyu/jty/system/entity/User.java
+++ src/main/java/com/tianyu/jty/system/entity/User.java
@@ -252,6 +252,12 @@ public class User implements java.io.Serializable {
 		this.delFlag = delFlag;
 	}
 
+	/*
+     * cascade：为级联操作，里面有级联保存，级联删除等，all为所有 
+     * fetch：加载类型，有lazy和eager二种，
+     *   eager为急加载，意为立即加载，在类加载时就加载，lazy为慢加载，第一次调用的时候再加载，由于数据量太大，onetomany一般为lazy
+     * mappedBy：这个为manytoone中的对象名，这个不要变哦
+     */
 	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "user")
 	public Set<UserRole> getUserRoles() {
 		return this.userRoles;
